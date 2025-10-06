75b5399f3bd162b9126126476055a241a0f85908
Gabriel Gibb <ggibb@turionspace.com>
2025-10-05 18:23:56 -0700

    asd

--- test-three/vite.config.ts
+++ test-three/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gorb-bro/', // Set base path for GitHub Pages
+  base: '/', // Set base path for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
