fafbbf6bcc65f51f597e8ed766d5b1181dab6a84
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2025-10-05 20:16:38 -0700

    static builder

--- test-three/vite.config.ts
+++ test-three/vite.config.ts
@@ -1,13 +1,27 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Set base path for GitHub Pages
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
-})
+    plugins: [
+        react(),
+        viteSingleFile({
+            removeViteModuleLoader: true,
+            useRecommendedBuildConfig: true,
+            deleteInlinedFiles: true,
+            inlinePattern: ["**/*.js", "**/*.css", "**/*.html"],
+        }),
+    ],
+    base: "/", // Set base path for GitHub Pages
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                assetFileNames: "assets/[name].[ext]",
+            },
+        },
+    },
+});
